82ebe368d74facf824cca8a3859178fe1201daf5
nSedrickm <nsedrick101@gmail.com>
2019-06-23 08:36:41 +0100

    colored

---
+++ color.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+#writing output in color 
+
+echo -e "\e[1;30m This is black text \e[0m"
+echo -e "\e[1;31m This is red text \e[0m"
+echo -e "\e[1;32m This is Green text \e[0m"
+echo -e "\e[1;33m This is yellow text \e[0m"
+echo -e "\e[1;34m This is blue text \e[0m"
+echo -e "\e[1;35m This is magenta text \e[0m"
+echo -e "\e[1;36m This is Cyan text \e[0m"
+echo -e "\e[1;37m This is white text \e[0m"
+
+echo  ------------------------------------
+
+
+echo -e "\e[1;40 ===================== \e[0"
+
+echo -e "\e[1;41 ===================== \e[0"
+
+echo -e "\e[1;42 ===================== \e[0"
+
+echo -e "\e[1;43 ===================== \e[0"
+
+echo -e "\e[1;44 ===================== \e[0"
+
+echo -e "\e[1;45 ===================== \e[0"
+
+echo -e "\e[1;46 ===================== \e[0"
+
+echo -e "\e[1;47 ===================== \e[0"
