140b7959754674d34e776e132bf5c0f510890382
nSedrickm <nsedrick101@gmail.com>
2019-06-28 22:25:12 +0100

    +1

--- arrays.sh
+++ arrays.sh
@@ -5,7 +5,9 @@ array=(1 2 3 4 5 6 7)
 array[0]="name"
 array[1]="food"
 array[2]="other-stuff"
-
+array[5]=3
+array[6]=6
 echo array_elements 
 echo ${array[*]} 
 echo entries : ${#array[*]} 
+echo "${array[5]} + ${array[6]}" | bc
